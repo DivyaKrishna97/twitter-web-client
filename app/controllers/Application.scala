1442b05d3b0d3f8bc4b8a1cf6ef28096c560a024
Douglas Teoh <douglas@dteoh.com>
2013-11-30 09:38:02 +1000

    Update root route
    * redirect to protected area if logged in
    * show sign in link otherwise

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import securesocial.core.SecureSocial
 
-object Application extends Controller {
+object Application extends Controller with SecureSocial {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = UserAwareAction { implicit request =>
+    request.user match {
+      case Some(user) => Redirect(routes.Protected.index)
+      case _ => Ok(views.html.index())
+    }
   }
 
 }
