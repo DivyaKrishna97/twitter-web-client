809469ffcb9fbc4a0ae8d74b2cf1acc16dd1ea5f
Douglas Teoh <douglas@dteoh.com>
2013-11-30 08:54:11 +1000

    Print out token and secret after authenticating.

--- app/controllers/Protected.scala
+++ app/controllers/Protected.scala
@@ -7,7 +7,12 @@ import securesocial.core.SecureSocial
 object Protected extends Controller with SecureSocial {
 
   def index = SecuredAction { implicit request =>
-    Ok(request.user.fullName)
+    val user = request.user
+    val (token, secret) = user.oAuth1Info match {
+      case Some(info) => (info.token, info.secret)
+      case None => (null, null)
+    }
+    Ok(s"Name: ${user.fullName} Token: $token Secret: $secret")
   }
 
 }
