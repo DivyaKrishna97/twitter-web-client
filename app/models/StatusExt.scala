2ab1c4c23dea124f764f2463738ed46760f2fa7b
Douglas Teoh <douglas@dteoh.com>
2013-12-01 14:35:44 +1000

    Display retweets properly.

--- app/models/StatusExt.scala
+++ app/models/StatusExt.scala
@@ -7,14 +7,18 @@ import twitter4j._
 class StatusExt(val status: Status) {
 
   def toJson = {
-    val user = status.getUser
+    val message = if (status.isRetweet) status.getRetweetedStatus else status
+    val user = message.getUser
+
     Json.toJson(
       Map(
-        "id" -> Json.toJson(status.getId.toString),
-        "message" -> Json.toJson(status.getText),
+        "id" -> Json.toJson(message.getId.toString),
+        "message" -> Json.toJson(message.getText),
         "author" -> Json.toJson(user.getName),
         "screenName" -> Json.toJson(user.getScreenName),
-        "avatarUrl" -> Json.toJson(user.getProfileImageURL)
+        "avatarUrl" -> Json.toJson(user.getProfileImageURL),
+        "retweet" -> Json.toJson(status.isRetweet),
+        "retweetedBy" -> Json.toJson(if (status.isRetweet) status.getUser.getName else null)
       )
     )
   }
